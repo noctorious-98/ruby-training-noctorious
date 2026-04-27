5dfd90a54d87c9efcf5e9bef01030273bcf36f6f
noctorious-98 <michael.noctor@gmail.com>
2026-04-27 13:57:57 +0000

    open_classes_spec answers

--- the-language/spec/2-medium/open_classes_spec.rb
+++ the-language/spec/2-medium/open_classes_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'Classes can be opened in ruby' do
 
   it 'eats as defined' do
     hammy = Hamster.new
-    expect(hammy.eat).to eq(__)
+    expect(hammy.eat).to eq('MUNCH!')
   end
 
   # Open the existing Hamster class and add a new method.
@@ -21,8 +21,8 @@ RSpec.describe 'Classes can be opened in ruby' do
 
   it 'can now nibble too' do
     hammy = Hamster.new
-    expect(hammy.nibble).to eq(__)
-    expect(hammy.eat).to eq(__)
+    expect(hammy.nibble).to eq('mmmm')
+    expect(hammy.eat).to eq('MUNCH!')
   end
 
   class Integer
@@ -32,8 +32,8 @@ RSpec.describe 'Classes can be opened in ruby' do
   end
 
   it 'is possible to reopen built in classes' do
-    expect(1.even_number?).to eq(__)
-    expect(2.even_number?).to eq(__)
+    expect(1.even_number?).to eq(false)
+    expect(2.even_number?).to eq(true)
   end
 
   it 'nibbles differently' do
@@ -45,8 +45,8 @@ RSpec.describe 'Classes can be opened in ruby' do
     end
 
     hammy = Hamster.new
-    expect(hammy.nibble).to eq(__)
-    expect(hammy.eat).to eq(__)
+    expect(hammy.nibble).to eq('i am nibbling differently now')
+    expect(hammy.eat).to eq('MUNCH!')
   end
 
   # Open an existing standard class and rewrite a method.
@@ -57,10 +57,10 @@ RSpec.describe 'Classes can be opened in ruby' do
   end
 
   it 'alters behaviour for our instances of the modified class' do
-    expect('String'.itself).to eq(__)
-    expect(1.itself).to eq(__)
-    expect(:symbol.itself).to eq(__)
-    expect('A very long string'.itself).to eq(__)
+    expect('String'.itself).to eq('Well I am myself of course!')
+    expect(1.itself).to eq(1)
+    expect(:symbol.itself).to eq(:symbol)
+    expect('A very long string'.itself).to eq("Well I am myself of course!")
   end
 
   # Open all object types and rewrite a method.
@@ -71,7 +71,7 @@ RSpec.describe 'Classes can be opened in ruby' do
   end
 
   it 'can have very strange behaviour for classes' do
-    expect(Hamster.new.to_s).to eq(__)
-    expect("Hello to you Mr. #{Hamster.new}").to eq(__)
+    expect(Hamster.new.to_s).to eq('Interpolation will be affected...')
+    expect("Hello to you Mr. #{Hamster.new}").to eq("Hello to you Mr. Interpolation will be affected...")
   end
 end
